f08339a19f40963dd95fe0f249d46b80aa5721e7
RolandStrod <KhaleAldeAsad@gmail.com>
2020-11-30 15:47:01 +0200

    Hamster

---
+++ ClassChallenges/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassChallenges
+{
+    class Program
+    {
+        class Cat
+        {
+            string  name;
+            string color;
+            double hunger;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hunger = 0;
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public double Hunger
+            {
+                get { return hunger; }
+            }
+
+            public void Sleep()
+            {
+                hunger += 0.2;
+            }
+
+        }
+
+       
+       
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("kitty", "black");
+            Console.WriteLine("You have A Cat!");
+            while (newCat.Hunger != 0)
+            {
+               
+            }
+
+            newCat.Sleep();
+            if (newCat.Hunger != 1)
+            {
+                Console.WriteLine("Meow");
+            }
+
+
+        }
+    }
+}
---
+++ HamsterLifeCycle/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace HamsterLifeCycle
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            double weight;
+            int age;
+            int lifespan;
+
+            public Pet(string _name)
+            {
+                name = _name;
+                age = 0;
+                weight = 0.2;
+                lifespan = 10;
+
+
+            }
+            
+            public int Age
+            {
+                get { return age; }
+            }
+
+            public double Weight
+            {
+                get { return weight; }
+            }
+
+            public int LifeSpan
+            {
+                get { return lifespan; }
+            }
+
+
+               
+            public void Eating()
+            {
+                Console.WriteLine("nom-nom");
+                weight += 0.2;
+            }
+
+            public void WorkOut()
+            {
+                weight -= 0.1;
+                Console.WriteLine("Run Hamster Run!!");
+            }
+
+            public void LiveAYear()
+            {
+                age += 1;
+            }
+
+
+            public void ShowPetInfo()
+            {
+                Console.WriteLine($"Name: {name}, Age: {age}, Weight: {weight}, LifeSpan: {lifespan}");
+            }
+
+
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Pet newPet = new Pet("Hamster");
+            for(int i = 0; i < 10; i++) 
+            {
+                newPet.LiveAYear();
+                newPet.Eating();
+                if(newPet.Weight >= 0.7)
+                {
+                    newPet.WorkOut();
+                }
+
+            }
+            newPet.ShowPetInfo();
+            Console.WriteLine("GGWP");
+        }
+    }
+}
